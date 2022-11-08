23236a29270003f98c9321dddda831c0bb1db131
ivannikolaichenko <ivnik.cat@gmail.com>
2022-11-08 22:58:52 +0200

    Price counting improved

--- src/main/java/ua/edu/ucu/apps/demo/order/Order.java
+++ src/main/java/ua/edu/ucu/apps/demo/order/Order.java
@@ -2,6 +2,7 @@ package ua.edu.ucu.apps.demo.order;
 
 import lombok.*;
 import ua.edu.ucu.apps.demo.flower.FlowerBucket;
+import ua.edu.ucu.apps.demo.flower.FlowerPack;
 
 import java.util.List;
 
@@ -13,5 +14,7 @@ public class Order {
 
     private int id;
     private List<FlowerBucket> flowerBuckets;
-
+    public double getPrice() {
+        return flowerBuckets.stream().mapToDouble(FlowerBucket::getPrice).sum();
+    }
 }
